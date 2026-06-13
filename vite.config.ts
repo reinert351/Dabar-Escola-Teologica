a3e293e13ac4cbf0d04937c48af53a2b3608b029
reinert351 <82279823+reinert351@users.noreply.github.com>
2026-06-12 21:11:50 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,14 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
-  return {
-    base: '/Dabar-Escola-Teologica/',
+export default defineConfig({
+  base: '/Dabar-Escola-Teologica/',
 
-    plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
 
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
-    },
-  };
+  },
 });
